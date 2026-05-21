72430ae7d8b8190899f3160603e312ae44256e5a
Hos2009 <amezianhosaine20@gmail.com>
2026-05-21 15:52:16 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'Ahfad Al-Khair',description:'Asociación Humanitaria y Solidaria'}
+export default function RootLayout({children}:{children:React.ReactNode}){return(<html lang='es'><body>{children}</body></html>)}
