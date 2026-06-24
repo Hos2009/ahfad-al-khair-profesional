9a993f51afff4ee6bc1f4dae8d2c3c8214f15383
Hos2009 <amezianhosaine20@gmail.com>
2026-06-24 15:55:27 +0200

    Add files via upload

--- ahfad-al-khair-completo/ahfad-al-khair-completo/app/page.tsx
+++ ahfad-al-khair-completo/ahfad-al-khair-completo/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1 className="text-5xl font-black mt-6">Ahfad Al-Khair</h1>
         <p className="mt-4 text-xl">Asociación para la Salud y el Desarrollo Sostenible</p>
         <p>Guercif - Marruecos</p>
- 
+
         <div className="mt-8 flex justify-center gap-4 flex-wrap">
           <a href="https://wa.me/212658743564"
           className="bg-white text-emerald-900 px-6 py-3 rounded-2xl font-bold">
@@ -29,7 +29,6 @@ export default function Home() {
         <h2 className="text-4xl font-bold mt-12">Nuestros Proyectos</h2>
 
         <div className="grid md:grid-cols-3 gap-6 mt-8">
-          <img src="/images/logo.png"  className="rounded-3xl"/>
           <img src="/images/salud.jpg" className="rounded-3xl"/>
           <img src="/images/proyecto.jpg" className="rounded-3xl"/>
           <img src="/images/proyecto2.jpg" className="rounded-3xl"/>
